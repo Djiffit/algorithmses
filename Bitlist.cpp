eee8aaad550508d525fdfe75360a1bddbfe67453
Konsta Kutvonen <konsta@kutvonen.xyz>
2020-01-27 19:29:19 +0200

    Solving done for the list of bits problem thing happening

--- Bitlist.cpp
+++ Bitlist.cpp
@@ -1,44 +1,39 @@
+// Given a bit sequence of length n, simulate m bit flips and after each flip,
+// print the longest sequence of same bit in the new sequence n <= 2 * 10e5,
+// m <= 2 * 10e5
+
 #include <iostream>
-#include <set>
-#include <vector>
-#include <unordered_set>
 #include <queue>
+#include <set>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-struct Range
-{
-    int start;
-    int end;
-    size_t operator()(const Range &r) const
-    {
-        return r.start ^ r.end;
-    }
+struct Range {
+  int start;
+  int end;
+  size_t operator()(const Range &r) const {
+    return hash<long long>()(((long long)r.start) ^ (((long long)r.end) << 32));
+  }
 };
-namespace std
-{
-template <>
-struct hash<Range>
-{
-    inline size_t operator()(const Range &r) const
-    {
-        return r.start ^ r.end;
-    }
+namespace std {
+template <> struct hash<Range> {
+  inline size_t operator()(const Range &r) const {
+    return hash<long long>()(((long long)r.start) ^ (((long long)r.end) << 32));
+  }
 };
 } // namespace std
 
-bool operator<(const Range &lhs, const Range &rhs)
-{
-    return lhs.start < rhs.start && lhs.end < rhs.end;
+bool operator<(const Range &lhs, const Range &rhs) {
+  return lhs.start < rhs.start && lhs.end < rhs.end;
 }
 
-bool operator>(const Range &lhs, const Range &rhs)
-{
-    return lhs.start > rhs.start && lhs.end > rhs.end;
+bool operator>(const Range &lhs, const Range &rhs) {
+  return lhs.start > rhs.start && lhs.end > rhs.end;
 }
 
-bool operator==(const Range &lhs, const Range &rhs)
-{
-    return lhs.start == rhs.start && lhs.end == rhs.end;
+bool operator==(const Range &lhs, const Range &rhs) {
+  return lhs.start == rhs.start && lhs.end == rhs.end;
 }
 
 priority_queue<pair<int, Range>> que;
@@ -47,125 +42,98 @@ set<Range> ranges;
 long m, n;
 string digit;
 
-void swap_bit(int ind)
-{
-    // cout << "SWAPPING " << ind << '\n';
-    if (digit[ind] == '1')
-    {
-        digit[ind] = '0';
-        auto r = *ranges.find({ind, ind});
-        ranges.erase(r);
-        active_ranges.erase({r.start, r.end});
-
-        if (r.start != ind)
-        {
-            Range new_pair = {r.start, ind - 1};
-            active_ranges.insert(new_pair);
-            que.push({(ind - 1) - r.start, new_pair});
-            ranges.insert(new_pair);
-        }
-        if (r.end != ind)
-        {
-            Range new_pair = {ind + 1, r.end};
-            ranges.insert(new_pair);
-            active_ranges.insert(new_pair);
-            que.push({r.end - (ind + 1), new_pair});
-        }
-    }
-    else
-    {
-        digit[ind] = '1';
-        if (ind > 0 && digit[ind - 1] == '1' && ind < digit.size() - 1 && digit[ind + 1] == '1')
-        {
-            auto r = *ranges.find({ind - 1, ind - 1});
-            ranges.erase(r);
-            active_ranges.erase({r.start, r.end});
-
-            auto r2 = *ranges.find({ind + 1, ind + 1});
-            ranges.erase(r2);
-            active_ranges.erase({r2.start, r2.end});
-
-            Range new_pair = {r.start, r2.end};
-            active_ranges.insert(new_pair);
-            que.push({r2.end - r.start, new_pair});
-            ranges.insert(new_pair);
-        }
-        else if (ind > 0 && digit[ind - 1] == '1')
-        {
-            auto r = *ranges.find({ind - 1, ind - 1});
-            ranges.erase(r);
-            active_ranges.erase({r.start, r.end});
-
-            Range new_pair = {min(r.start, ind), max(r.end, ind)};
-            active_ranges.insert(new_pair);
-            que.push({max(r.end, ind) - min(r.start, ind), new_pair});
-            ranges.insert(new_pair);
-        }
-        else if (ind < digit.size() - 1 && digit[ind + 1] == '1')
-        {
-            auto r = *ranges.find({ind + 1, ind + 1});
-            ranges.erase(r);
-            active_ranges.erase({r.start, r.end});
-
-            Range new_pair = {min(r.start, ind), max(r.end, ind)};
-            active_ranges.insert(new_pair);
-            que.push({max(r.end, ind) - min(r.start, ind), new_pair});
-            ranges.insert(new_pair);
-        }
-        else
-        {
-            Range new_pair = {ind, ind};
-            active_ranges.insert(new_pair);
-            que.push({0, new_pair});
-            ranges.insert(new_pair);
-        }
-    }
+void remove_range(Range r) {
+  ranges.erase(r);
+  active_ranges.erase(r);
 }
 
-int main()
-{
-    cin >> digit >> m;
-    bool in_range = false;
-    int start;
+void add_range(int start, int end) {
+  ranges.insert({start, end});
+  active_ranges.insert({start, end});
+  que.push({end - start + 1, {start, end}});
+}
 
-    for (int i = 0; i < digit.size(); i++)
-    {
-        auto c = digit[i];
-        if (c == '1')
-        {
-            if (!in_range)
-            {
-                in_range = true;
-                start = i;
-            }
-        }
-        else
-        {
-            if (in_range)
-            {
-                in_range = false;
-                ranges.insert({start, i - 1});
-                active_ranges.insert({start, i - 1});
-                que.push({start - (i - 1), {start, i - 1}});
-            }
-        }
-    }
-    if (in_range)
-    {
-        ranges.insert({start, (int)digit.size() - 1});
-        active_ranges.insert({start, (int)digit.size() - 1});
-        que.push({start - ((int)digit.size() - 1), {start, (int)digit.size() - 1}});
-    }
+void swap_bit(int ind) {
+  auto r = *ranges.find({ind, ind});
+  remove_range(r);
 
-    for (auto i : ranges)
-    {
-        cout << i.start << " " << i.end << '\n';
-    }
-    // cout << "RANGES DONE " << '\n';
+  if (ind == r.start && ind == r.end) {
+    auto left = ranges.find({ind - 1, ind - 1});
+    auto right = ranges.find({ind + 1, ind + 1});
+    int begin, end;
+    if (left != ranges.end()) {
+      begin = left->start;
+      remove_range(*left);
+    } else
+      begin = ind;
 
-    for (int i = 0; i < m; i++)
-    {
-        cin >> n;
-        swap_bit(n);
+    if (right != ranges.end()) {
+      end = right->end;
+      remove_range(*right);
+    } else
+      end = ind;
+
+    add_range(begin, end);
+  } else if (ind != r.start && ind != r.end) {
+    add_range(r.start, ind - 1);
+    add_range(ind + 1, r.end);
+    add_range(ind, ind);
+  } else if (ind == r.start) {
+    add_range(ind + 1, r.end);
+    if (ind == 0) {
+      add_range(ind, ind);
+    } else {
+      auto other = *ranges.find({ind - 1, ind - 1});
+      remove_range(other);
+      add_range(other.start, ind);
     }
+  } else if (ind == r.end) {
+    add_range(r.start, ind - 1);
+    if (ind == digit.size() - 1) {
+      add_range(ind, ind);
+    } else {
+      auto other = *ranges.find({ind + 1, ind + 1});
+      remove_range(other);
+      add_range(ind, other.end);
+    }
+  }
+
+  if (digit[ind] == '1')
+    digit[ind] = '0';
+  else
+    digit[ind] = '1';
+
+  while (true) {
+    auto top = que.top();
+    if (active_ranges.find(top.second) != active_ranges.end()) {
+      cout << top.first << '\n';
+      break;
+    }
+    que.pop();
+  }
+}
+
+int main() {
+  cin.sync_with_stdio(0);
+  cin.tie(0);
+  active_ranges.reserve(50000);
+  cin >> digit >> m;
+  char curr = digit[0];
+  int start = 0;
+
+  for (int i = 0; i < digit.size(); i++) {
+    auto c = digit[i];
+    if (curr != c) {
+      add_range(start, i - 1);
+      start = i;
+      curr = c;
+    }
+  }
+
+  add_range(start, digit.size() - 1);
+
+  for (int i = 0; i < m; i++) {
+    cin >> n;
+    swap_bit(n - 1);
+  }
 }
